9275daafe212093442a7dba2d30408e57923f2e4
Jarory <Jarory13@gmail.com>
2017-03-27 21:01:55 -0400

    Created a build

---
+++ Temp/StagingArea/Data/il2cppOutput/UnityEngine_UnityEngine_BoxCollider2D948534547MethodDeclarations.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <stdint.h>
+#include <assert.h>
+#include <exception>
+
+// UnityEngine.BoxCollider2D
+struct BoxCollider2D_t948534547;
+
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UnityEngine_Vector22243707579.h"
+
+// UnityEngine.Vector2 UnityEngine.BoxCollider2D::get_size()
+extern "C"  Vector2_t2243707579  BoxCollider2D_get_size_m1527190803 (BoxCollider2D_t948534547 * __this, const MethodInfo* method) IL2CPP_METHOD_ATTR;
+// System.Void UnityEngine.BoxCollider2D::INTERNAL_get_size(UnityEngine.Vector2&)
+extern "C"  void BoxCollider2D_INTERNAL_get_size_m1417005942 (BoxCollider2D_t948534547 * __this, Vector2_t2243707579 * ___value0, const MethodInfo* method) IL2CPP_METHOD_ATTR;
